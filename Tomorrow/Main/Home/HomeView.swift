64d96af240b2cc9f8d5f73987ba0abf832251497
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-08-19 14:50:20 +0900

    Feat(home): 상단 바 구현

--- Tomorrow/Main/Home/HomeView.swift
+++ Tomorrow/Main/Home/HomeView.swift
@@ -9,60 +9,80 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            VStack(spacing: 0) {
-                // Process 타이틀
+        VStack(spacing: 0) {
+            ZStack(alignment: .bottom) {
+                Rectangle()
+                    .foregroundColor(.white)
+                    .frame(height: 108)
+                    .shadow(color: .shadowGray, radius: 2, x: 0, y: 1)
+                
                 HStack {
-                    Text("Process")
+                    Spacer()
+                    Text("Today's class")
                         .font(.pretendard(type: .semibold, size: 18))
                         .foregroundColor(.tomorrowBlueHigh)
                     Spacer()
                 }
-                .padding(.horizontal, 18)
-                
-                // 진행중인 과정 카드 scrollview
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 12) {
-                        ProcessCard(title: "A dementia prevention project",
-                                    presentProgress: 6, totalProgress: 24)
-                        .padding(.leading, 18)
-                        
-                        ForEach(1 ..< 4) { _ in
-                            ProcessCard(title: "A dementia prevention project",
-                                        presentProgress: 13, totalProgress: 19)
-                        }
-                        
-                        ProcessCard(title: "A dementia prevention project",
-                                    presentProgress: 6, totalProgress: 24)
-                        .padding(.trailing, 18)
-                    }
-                }
-                .padding(.top, 20)
-                .padding(.bottom, 34)
-                
-                // Scheduled title
-                HStack {
-                    Text("Scheduled")
-                        .font(.pretendard(type: .semibold, size: 18))
-                        .foregroundColor(.tomorrowBlueHigh)
-                    Spacer()
-                }
-                .padding(.horizontal, 18)
-                
-                // Scheduled 리스트
-                VStack(spacing: 12) {
-                    ForEach(1 ..< 6) { _ in
-                        ScheduledList(title: "A letter-finding program",
-                                      description: "Target: Preschoolers")
-                        .padding(.horizontal, 18)
-                    }
-                }
-                .padding(.vertical, 20)
+                .padding(.bottom, 18)
             }
+            
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 0) {
+                    // Process 타이틀
+                    HStack {
+                        Text("Process")
+                            .font(.pretendard(type: .semibold, size: 18))
+                            .foregroundColor(.tomorrowBlueHigh)
+                        Spacer()
+                    }
+                    .padding(.top, 28)
+                    .padding(.horizontal, 18)
+                    
+                    // 진행중인 과정 카드 scrollview
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 12) {
+                            ProcessCard(title: "A dementia prevention project",
+                                        presentProgress: 6, totalProgress: 24)
+                            .padding(.leading, 18)
+                            
+                            ForEach(1 ..< 4) { _ in
+                                ProcessCard(title: "A dementia prevention project",
+                                            presentProgress: 13, totalProgress: 19)
+                            }
+                            
+                            ProcessCard(title: "A dementia prevention project",
+                                        presentProgress: 6, totalProgress: 24)
+                            .padding(.trailing, 18)
+                        }
+                    }
+                    .padding(.top, 20)
+                    .padding(.bottom, 34)
+                    
+                    // Scheduled title
+                    HStack {
+                        Text("Scheduled")
+                            .font(.pretendard(type: .semibold, size: 18))
+                            .foregroundColor(.tomorrowBlueHigh)
+                        Spacer()
+                    }
+                    .padding(.horizontal, 18)
+                    
+                    // Scheduled 리스트
+                    VStack(spacing: 12) {
+                        ForEach(1 ..< 6) { _ in
+                            ScheduledList(title: "A letter-finding program",
+                                          description: "Target: Preschoolers")
+                            .padding(.horizontal, 18)
+                        }
+                    }
+                    .padding(.vertical, 20)
+                }
+            }
+            .background(
+                Color.backgroundWhite
+            )
         }
-        .background(
-            Color.backgroundWhite
-        )
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
