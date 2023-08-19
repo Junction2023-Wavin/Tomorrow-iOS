bf70b212abeab8e045437e65fbc41e7021c81b07
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-08-19 14:23:20 +0900

    Feat(home): Scheduled List component 구현

--- Tomorrow/Main/Home/HomeView.swift
+++ Tomorrow/Main/Home/HomeView.swift
@@ -19,27 +19,27 @@ struct HomeView: View {
                     Spacer()
                 }
                 .padding(.horizontal, 18)
-
+                
                 // 진행중인 과정 카드 scrollview
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack(spacing: 12) {
                         ProcessCard(title: "A dementia prevention project",
                                     presentProgress: 6, totalProgress: 24)
-                            .padding(.leading, 18)
-
+                        .padding(.leading, 18)
+                        
                         ForEach(1 ..< 4) { _ in
                             ProcessCard(title: "A dementia prevention project",
                                         presentProgress: 13, totalProgress: 19)
                         }
-
+                        
                         ProcessCard(title: "A dementia prevention project",
                                     presentProgress: 6, totalProgress: 24)
-                            .padding(.trailing, 18)
+                        .padding(.trailing, 18)
                     }
                 }
                 .padding(.top, 20)
                 .padding(.bottom, 34)
-
+                
                 // Scheduled title
                 HStack {
                     Text("Scheduled")
@@ -49,52 +49,20 @@ struct HomeView: View {
                 }
                 .padding(.horizontal, 18)
                 
+                // Scheduled 리스트
                 VStack(spacing: 12) {
                     ForEach(1 ..< 6) { _ in
                         ScheduledList(title: "A letter-finding program",
                                       description: "Target: Preschoolers")
-                            .padding(.horizontal, 18)
+                        .padding(.horizontal, 18)
                     }
                 }
-                .padding(.top, 20)
+                .padding(.vertical, 20)
             }
         }
         .background(
             Color.backgroundWhite
         )
-    }
-}
-
-struct ScheduledList:View {
-    let title: String
-    let description: String
-    
-    var body: some View {
-        ZStack {
-            Rectangle()
-                .foregroundColor(.white)
-                .frame(height: 90)
-                .cornerRadius(20)
-                .shadow(color: Color.shadowGray, radius: 5, x: 0, y: 0)
-            
-            HStack {
-                VStack(alignment: .leading, spacing: 4) {
-                    Text(title)
-                        .font(.pretendard(type: .medium, size: 17))
-                        .foregroundColor(.tomorrowGray)
-                    Text(description)
-                        .font(.pretendard(type: .extraLight, size: 12))
-                        .foregroundColor(.tomorrowGray)
-                }
-                .padding(.leading, 32)
-                
-                Spacer()
-                
-                Image(systemName: "chevron.right")
-                    .frame(width: 13, height: 22)
-                    .padding(.trailing, 28)
-            }
-        }
     }
 }
 
---
+++ Tomorrow/Main/Home/ScheduledList.swift
@@ -0,0 +1,41 @@
+//
+//  ScheduledList.swift
+//  Tomorrow
+//
+//  Created by 이안진 on 2023/08/19.
+//
+
+import SwiftUI
+
+struct ScheduledList: View {
+    let title: String
+    let description: String
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .frame(height: 90)
+                .cornerRadius(20)
+                .shadow(color: Color.shadowGray, radius: 5, x: 0, y: 0)
+            
+            HStack {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(title)
+                        .font(.pretendard(type: .medium, size: 17))
+                        .foregroundColor(.tomorrowGray)
+                    Text(description)
+                        .font(.pretendard(type: .extraLight, size: 12))
+                        .foregroundColor(.tomorrowGray)
+                }
+                .padding(.leading, 32)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+                    .frame(width: 13, height: 22)
+                    .padding(.trailing, 28)
+            }
+        }
+    }
+}
