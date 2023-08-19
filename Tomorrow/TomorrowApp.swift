148ed53acf4a3e7a675ac0c489d7e1149e2e91d6
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-08-19 10:48:36 +0900

    first commit

---
+++ Tomorrow/TomorrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomorrowApp.swift
+//  Tomorrow
+//
+//  Created by 이안진 on 2023/08/19.
+//
+
+import SwiftUI
+
+@main
+struct TomorrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
